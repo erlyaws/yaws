c3dd5960e0b5029c61a312f9e0579b77dab0fd8d
Claes Wikstrom <klacke@hyber.org>
2002-11-25 21:45:40 +0000

    ""
    git-svn-id: https://erlyaws.svn.sourceforge.net/svnroot/erlyaws/trunk/yaws@302 9fbdc01b-0d2c-0410-bfb7-fb27d70d8b52

--- src/yaws_ctl.erl
+++ src/yaws_ctl.erl
@@ -21,8 +21,11 @@
 -define(F, "/tmp/yaws.ctl").
 
 
+ctl_file("0") ->
+    io_lib:format("/var/run/yaws.ctl");
 ctl_file(Id) ->
     io_lib:format("/tmp/yaws.ctl.~s",[Id]).
+
 
 start(_Top, Id) ->
     case gen_tcp:listen(0, [{packet, 2},
