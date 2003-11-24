503738664a39ceca05e4dde0db6a91cb264856db
Claes Wikstrom <klacke@hyber.org>
2003-11-24 23:50:16 +0000

    ""
    git-svn-id: https://erlyaws.svn.sourceforge.net/svnroot/erlyaws/trunk/yaws@549 9fbdc01b-0d2c-0410-bfb7-fb27d70d8b52

---
+++ www/ybed.erl
@@ -0,0 +1,26 @@
+%%%-------------------------------------------------------------------
+%%% File    : ybed.erl
+%%% Author  :  <klacke@hyber.org>
+%%% Description : Small embedded yaws example
+%%%
+%%% Created : 25 Nov 2003 by  <klacke@hyber.org>
+%%%-------------------------------------------------------------------
+-module(ybed).
+-compile(export_all).
+
+-include("/usr/local/lib/yaws/include/yaws.hrl").
+
+start() ->
+    application:start(yaws),
+    GC = yaws_config:make_default_gconf(false),
+    SC = #sconf{access_log = true,
+		port = 8888,
+		servername = "foobar",
+		listen = {0,0,0,0},
+		docroot = "/tmp"},
+    yaws_api:setconf(GC, [[SC]]).
+
+		
+
+    
+
