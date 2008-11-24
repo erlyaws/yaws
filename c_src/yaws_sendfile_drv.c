1ede2f742bbb961419c3be167da82fc6a31f67e3
Steve Vinoski <vinoski@ieee.org>
2008-11-24 14:57:08 +0000

    open input file non-blocking and handle ready_input on it
    git-svn-id: https://erlyaws.svn.sourceforge.net/svnroot/erlyaws/trunk/yaws@1294 9fbdc01b-0d2c-0410-bfb7-fb27d70d8b52

--- c_src/yaws_sendfile_drv.c
+++ c_src/yaws_sendfile_drv.c
@@ -29,6 +29,8 @@ typedef struct _desc {
     off_t offset;
     size_t count;
     ssize_t total;
+    unsigned int write_ready: 1;
+    unsigned int read_ready:  1;
 } Desc;
 
 static ErlDrvData yaws_sendfile_drv_start(ErlDrvPort port, char* buf)
@@ -37,7 +39,7 @@ static ErlDrvData yaws_sendfile_drv_start(ErlDrvPort port, char* buf)
     if (d == NULL) return (ErlDrvData) -1;
     d->port = port;
     d->socket_fd = d->file_fd = -1;
-    d->offset = d->count = d->total = 0;
+    d->offset = d->count = d->total = d->write_ready = d->read_ready = 0;
     return (ErlDrvData)d;
 }
 
@@ -119,7 +121,7 @@ static void yaws_sendfile_drv_output(ErlDrvData handle, char* buf, int buflen)
     Desc* d = (Desc*)handle;
     Buffer b;
     b.buffer = buf;
-    fd = open(b.args->filename, O_RDONLY);
+    fd = open(b.args->filename, O_RDONLY | O_NONBLOCK);
     if (fd < 0) {
         out_buflen = set_error_buffer(&b, errno);
     } else {
@@ -127,6 +129,7 @@ static void yaws_sendfile_drv_output(ErlDrvData handle, char* buf, int buflen)
         d->file_fd = fd;
         d->offset = b.args->offset.offset;
         d->count = b.args->count.size;
+        driver_select(d->port, (ErlDrvEvent)d->file_fd, DO_READ, 1);
         driver_select(d->port, (ErlDrvEvent)d->socket_fd, DO_WRITE, 1);
     }
     if (out_buflen != 0) {
@@ -134,9 +137,8 @@ static void yaws_sendfile_drv_output(ErlDrvData handle, char* buf, int buflen)
     }
 }
 
-static void yaws_sendfile_drv_ready_output(ErlDrvData handle, ErlDrvEvent ev)
+static void yaws_sendfile_do_send(Desc* d)
 {
-    Desc* d = (Desc*)handle;
     off_t cur_offset = d->offset;
     ssize_t result;
     result = yaws_sendfile_call(d->socket_fd, d->file_fd, &d->offset, d->count);
@@ -144,13 +146,13 @@ static void yaws_sendfile_drv_ready_output(ErlDrvData handle, ErlDrvEvent ev)
         off_t written = d->offset - cur_offset;
         d->count -= written;
         d->total += written;
-        /*driver_select(d->port, (ErlDrvEvent)d->socket_fd, DO_WRITE, 1);*/
     } else {
         int out_buflen;
         char buf[32];
         Buffer b;
         b.buffer = buf;
         memset(buf, 0, sizeof buf);
+        driver_select(d->port, (ErlDrvEvent)d->file_fd, DO_READ, 0);
         driver_select(d->port, (ErlDrvEvent)d->socket_fd, DO_WRITE, 0);
         close(d->file_fd);
         if (result < 0) {
@@ -165,6 +167,25 @@ static void yaws_sendfile_drv_ready_output(ErlDrvData handle, ErlDrvEvent ev)
         d->offset = d->count = d->total = 0;
         driver_output(d->port, buf, out_buflen);
     }
+    d->write_ready = d->read_ready = 0;
+}
+
+static void yaws_sendfile_drv_ready_input(ErlDrvData handle, ErlDrvEvent ev)
+{
+    Desc* d = (Desc*)handle;
+    d->read_ready = 1;
+    if (d->write_ready) {
+        yaws_sendfile_do_send(d);
+    }
+}
+
+static void yaws_sendfile_drv_ready_output(ErlDrvData handle, ErlDrvEvent ev)
+{
+    Desc* d = (Desc*)handle;
+    d->write_ready = 1;
+    if (d->read_ready) {
+        yaws_sendfile_do_send(d);
+    }
 }
 
 static ErlDrvEntry yaws_sendfile_driver_entry = {
@@ -172,7 +193,7 @@ static ErlDrvEntry yaws_sendfile_driver_entry = {
     yaws_sendfile_drv_start,
     yaws_sendfile_drv_stop,
     yaws_sendfile_drv_output,
-    NULL,
+    yaws_sendfile_drv_ready_input,
     yaws_sendfile_drv_ready_output,
     "yaws_sendfile_drv",
     NULL,
