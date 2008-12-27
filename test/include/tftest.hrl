a919ac1246c83aedea60e3a0a6f0a3f3b323fca0
Claes Wikstrom <klacke@hyber.org>
2008-12-27 16:01:27 +0000

    git-svn-id: https://erlyaws.svn.sourceforge.net/svnroot/erlyaws/trunk/yaws@1321 9fbdc01b-0d2c-0410-bfb7-fb27d70d8b52

---
+++ test/include/tftest.hrl
@@ -0,0 +1,2 @@
+-define(line, put('$line',{?MODULE,?LINE}),).
+
---
+++ test/t2/app_test.erl
@@ -0,0 +1,26 @@
+-module(app_test).
+-include("../include/tftest.hrl").
+-include("../ibrowse/src/ibrowse.hrl").
+-compile(export_all).
+
+start() ->
+    ?line ok,
+    test1(),
+    test2(),
+    test3().
+
+
+test1() ->
+    ?line ok.
+
+test2() ->
+    ?line ok.
+
+test3() ->
+    ?line ok.
+
+
+
+    
+
+
