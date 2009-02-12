fdfdd8edf4b8b1c71c6536ad56043a2cb1fb4596
Claes Wikstrom <klacke@hyber.org>
2009-02-12 22:14:40 +0000

    prep for 1.80
    git-svn-id: https://erlyaws.svn.sourceforge.net/svnroot/erlyaws/trunk/yaws@1388 9fbdc01b-0d2c-0410-bfb7-fb27d70d8b52

--- win32/README.txt
+++ win32/README.txt
@@ -1,2 +1,22 @@
 
 win32 version of yaws
+
+For just running yaws, it works pretty much the same as on UNIX.
+Commands that don't work are --id, --heart and some more
+Try yaws --help for available options.
+
+Just starting
+
+> yaws -i 
+
+Starts a basic yaws system with a yaws.conf taken from
+the install dir.
+
+It's actually possible to develop and edit yaws inside windows. 
+Required tools are unxutils and perl installed.
+
+
+Enjoy,
+
+/klacke
+
--- win32/yaws.c
+++ win32/yaws.c
@@ -1,3 +1,7 @@
+/* win32 version of yaws starup script */
+/* Author: klacke@hyber.org (Jan 2009) */
+
+
 #include <windows.h>
 #include <stdio.h>
 
@@ -18,8 +22,7 @@ static void readreg() {
             exit(1);
         }
     memset(path, 0, BSIZ);
-    int ret = RegQueryValueEx(hKey, "DIR", NULL, NULL,
-                              path, &bsz);
+    int ret = RegQueryValueEx(hKey, "DIR", NULL, NULL, path, &bsz);
     fpath = (unsigned char*)malloc(strlen(path));
     int i;
     for(i = 0; i<strlen(path); i++ ) {
@@ -41,74 +44,13 @@ int charg(char **argv, char *s1, char *s2, int p) {
 }
 
 
-int main(int argc, char**argv) {
-
-    int interactive  = 0; 
-    int winteractive  = 0; 
-    char *conf = NULL;
-    int debug = 0;
-    char *runmod = NULL;
-
-    readreg();
-
-    int p = 1;
-    while (p < argc) {
-        if (charg(argv, "-i", "--interactive", p))  
-            interactive = 1;
-        else if (charg(argv, "-w", "--winteractive", p))
-            winteractive = 1;
-        else if (charg(argv, "--debug", NULL, p))
-            debug = 1;
-        else if (charg(argv, "-c", "--conf", p)) 
-            conf = argv[++p];
-        else if (charg(argv, "--runmod", NULL, p))
-            runmod = argv[++p];
-        else {
-            fprintf(stderr, "Unknown arg: %s\n", argv[p]);
-            exit(1);
-        }
-        p++;
-    }
-    printf("interactive = %d\n"
-           "winteractive = %d\n"
-           "conf = %s\n"
-           "debug = %d\n"
-           "runmod = %s\n", 
-           interactive, winteractive,
-           conf, debug, runmod);
-
-
+int runwait(char *execString) {
+    
     STARTUPINFO          si = { sizeof(si) };
     PROCESS_INFORMATION  pi;
-    char                 szExe[] = "erl.exe -s test test";
-    char                 execString[BSIZ];
-    char                 paBuf[BSIZ];
-    int i;
-    int len;
-    char yawsargs[BUFSIZ];
 
-    if (winteractive)
-        sprintf(execString, "werl.exe  ");
-    else
-        sprintf(execString, "erl.exe  ");
+    //printf("execString: %s \n", execString);
 
-    sprintf(paBuf, " -pa \"%s/ebin\" ", fpath);
-    strcat(execString, paBuf);
-
-    if (debug)
-        strcat(execString, " -boot start_sasl -yaws debug ");
-    if (interactive == 0 && winteractive == 0)
-        strcat(execString, " --detach ");
-
-    if (conf == NULL) 
-        sprintf(yawsargs, " -conf \"%s\\yaws.conf\" ", path);
-    else 
-        sprintf(yawsargs, " -conf \"%s\" ", conf);
-    strcat(execString, yawsargs);
-    strcat(execString, " -run yaws -yaws id default ");
-    
-
-    printf("execString = %s\n", execString);
     if(CreateProcess(0, execString, NULL, NULL, FALSE, 0, 0, 0, &si, &pi))
         {
             unsigned long ret = 0;
@@ -123,6 +65,193 @@ int main(int argc, char**argv) {
         }
     printf("Failed to create the process entirely\n");
     return 1;
+}
+
+
+
+int nosh(char *s) {
+    char buf[BUFSIZ];
+    sprintf (buf, "erl -noshell -pa \"%s/ebin\" %s ", fpath, s);
+    return runwait(buf);
+}
+
+int help() {
+    printf("usage:  "
+""
+"       yaws -i | --interactive       -- interactive (no daemon) mode"
+"       yaws -w | --winteractive      -- interactive (werl) "
+"       yaws --daemon                 -- daemon mode"
+""
+""
+""
+"       Auxiliary flags for the daemon: "
+"            --id Id          --  set system id"
+"            --debug          --  debug mode "
+"            --conf File      --  set config file"
+"            --tracetraf      --  trace traffic"
+"            --tracehttp      --  trace http traffic"
+"            --traceout       --  trace output to stdout"
+"            --version        --  print version"
+"            --pa path        --  add load path"
+"            --mnesiadir dir  --  start Mnesia in dir"
+"            --sname xxx      --  start with sname xxx"
+"            --name xxx       --  start with name xxx"
+"            --runmod mod     --  call mod:start/0 at startup"
+"            --erlarg X       --  pass argument X to $erl"
+""
+"ctl functions ... "
+"        yaws --hup [--id ID]                 -- hup the daemon, reload conf"
+"        yaws --stop [--id ID]                -- stop the daemon "
+"        yaws --debug-dump [--id ID]          -- produce a debug dump "
+"        yaws --status [--id ID]              -- query the daemon status "
+"        yaws --load Modules                  -- load modules "
+"        yaws --ls                            -- list Yaws nodes and their status"
+"        yaws --ctltrace traffic|http         -- toggle trace of running daemon");
+    
+    return(0);
+}
+
+
+
+int main(int argc, char**argv) {
+
+    int interactive  = 1; 
+    int winteractive  = 0; 
+    int daemon = 0;
+    char *conf = NULL;
+    char *sname = NULL;
+    char *name = NULL;
+    int debug = 0;
+    char *runmod = NULL;
+    char *trace = NULL;
+    char *traceoutput = NULL;
+    char tbuf[BUFSIZ];
+    char paBuf[BUFSIZ];
+    char execString[BSIZ];
+    char mnesia[255];
+    char erlarg[255];
+    char *id = "default";
+
+    memset(paBuf, 0, BUFSIZ);
+    memset(tbuf, 0, BUFSIZ);
+    memset(execString, 0, BSIZ);
+
+    readreg();
+    sprintf(paBuf, " -pa \"%s/ebin\" ", fpath);
+
+    int p = 1;
+    while (p < argc) {
+        if (charg(argv, "-i", "--interactive", p))   {
+            interactive = 1;
+        }
+        else if (charg(argv, "-w", "--winteractive", p)) {
+            winteractive = 1;
+        }
+        else if (charg(argv, "-D", "--daemon", p)) {
+            interactive = winteractive = 0;
+            daemon = 1;
+        }
+        else if (charg(argv, "--debug", NULL, p)) {
+            debug = 1;
+        }
+        else if (charg(argv, "-t", "--tracetraf", p)) {
+             trace=" -yaws trace traffic ";
+        }
+        else if (charg(argv, "-T", "--tracehttp", p)) {
+             trace=" -yaws trace http ";
+        }
+        else if (charg(argv, "-x", "--traceout", p)) {
+            traceoutput = " -yaws traceoutput ";
+        }
+        else if (charg(argv, "--trace", NULL, p)) {
+            traceoutput = " -yaws traceoutput ";
+            trace = " -yaws trace traffic ";
+        }
+        else if (charg(argv, "--mnesiadir", "-M", p)) {
+            sprintf(mnesia, "-mnesia dir \"%s\" -run mnesia start ",argv[++p]);
+        }
+        else if (charg(argv, "-c", "--conf", p))  {
+            conf = argv[++p];
+        }
+        else if (charg(argv, "--id", NULL, p))  {
+            id = argv[++p];
+        }
+        else if (charg(argv, "-pa", NULL, p))  {
+            sprintf(tbuf, " -pa \"%s\" ", argv[++p]);
+            strcat(paBuf, tbuf);
+        }
+        else if (charg(argv, "--runmod", "-r", p)) {
+            runmod = argv[++p];
+        }
+        else if (charg(argv, "--hup", "-h", p)) {
+            return nosh("-s yaws_ctl hup default");
+        }
+        else if (charg(argv, "--stop", "-s", p)) {
+            return nosh("-s yaws_ctl stop default");
+        }
+        else if (charg(argv, "--status", "-S", p)) {
+            return nosh("-s yaws_ctl status default");
+        }
+        else if (charg(argv, "--debug-dump", NULL, p)) {
+            return nosh("-s yaws_ctl debug_dump default ");
+        }
+        else   if (charg(argv, "--version", "-v", p)) {  
+            nosh("-s yaws printversion");
+            exit(0);
+        }
+        else   if (charg(argv, "--sname", "-sname", p)) {  
+             sname = argv[++p];
+        }
+        else   if (charg(argv, "--name", "-name", p)) {  
+             name = argv[++p];
+        }
+        else   if (charg(argv, "--erlarg", "-erlarg", p)) {  
+            sprintf(tbuf, " %s ", argv[++p]);
+            strcat(erlarg, tbuf);
+        }
+        else {
+            return help();
+        }
+        p++;
+    }
+
+    if (winteractive)
+        sprintf(execString, "werl.exe  %s %s ", erlarg, paBuf);
+    else
+        sprintf(execString, "erl.exe  %s %s ", erlarg, paBuf);
+
+    if (debug)
+        strcat(execString, " -boot start_sasl -yaws debug ");
+    if ((interactive == 0 && winteractive == 0) || daemon == 1)
+        strcat(execString, " -detached ");
+    
+    if (conf == NULL) 
+        sprintf(tbuf, " -conf \"%s\\yaws.conf\" ", path);
+    else 
+        sprintf(tbuf, " -conf \"%s\" ", conf);
+    strcat(execString, tbuf);
+    strcat(execString, " -run yaws -yaws id default ");
+
+    if (mnesia != NULL) 
+        strcat(execString, mnesia);
+    if (trace != NULL) 
+        strcat(execString, trace);
+    if (traceoutput != NULL)
+        strcat(execString, traceoutput);
+
+    if (sname != NULL) {
+        sprintf(tbuf," -sname %s ", sname);
+        strcat(execString, tbuf);
+    } else if (name != NULL) {
+        sprintf(tbuf, " -name %s ", name);
+        strcat(execString, tbuf);
+    } 
+    if (runmod != NULL) {
+        sprintf(tbuf, " -runmod %s ", runmod);
+        strcat(execString, tbuf);
+    }
+
+    return runwait(execString);
 }
 
                
