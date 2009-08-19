792697b07c24882ac483cc0bfee0ac136f7b9127
Claes Wikstrom <klacke@hyber.org>
2009-08-19 14:48:41 +0200

    possible bug with id check

--- src/yaws_config.erl
+++ src/yaws_config.erl
@@ -729,7 +729,7 @@ fload(FD, globals, GC, C, Cs, Lno, Chars) ->
                     {error, ?F("Expect true|false at line ~w", [Lno])}
             end;
         ["id", '=', String] when GC#gconf.id == undefined;  
-                                 GC#gconf.id == default ->
+                                 GC#gconf.id == "default" ->
             fload(FD, globals, GC#gconf{id=String},C, Cs, Lno+1, Next);
         ["id", '=', String]  ->
             error_logger:format("Ignoring 'id = ~p' setting at line ~p~n", 
