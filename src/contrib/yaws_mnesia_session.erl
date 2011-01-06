0761bb27c2edd6d05d5b792e6b79104f7a3cfcf7
Nicolas Thauvin <qux@free.fr>
2011-01-06 01:15:26 +0100

    Pluggable ysession storage with mnesia sample in src/contrib

---
+++ src/contrib/yaws_mnesia_session.erl
@@ -0,0 +1,97 @@
+%%%----------------------------------------------------------------------
+%%% File    : yaws_mnesia_session.erl
+%%% Author  : Nicolas Thauvin <nicolas@corporama.com>
+%%% Purpose : mnesia storage callbacks for yaws_session_server
+%%%----------------------------------------------------------------------
+
+%% Warning !!!
+%% This module may init mnesia in a way that is not suitable for your
+%% application !
+
+%% Quick steps:
+%% 1) Compile this module
+%% 2) Drop the .beam in your code path
+%% 3) Run Yaws with the 
+
+-module (yaws_mnesia_session).
+
+-export ([init_backend/1, stop_backend/0]).
+-export ([list/0, insert/1, lookup/1, delete/1]).
+-export ([traverse/1, cleanup/0]).
+
+-define(TABLE, ysession).
+
+init_backend(Fields) ->
+    case catch mnesia:table_info(schema, where_to_write) of
+        Nodes when is_list(Nodes) ->
+            case catch mnesia:table_info(?TABLE, where_to_write) of
+                {'EXIT', {aborted, {no_exists, ?TABLE, where_to_write}}} ->
+                    Nodes = mnesia:system_info(running_db_nodes),
+                    Options = [{disc_copies, Nodes}, {type, set},
+                               {attributes, Fields}],
+                    {atomic, ok} = mnesia: create_table (?TABLE, Options),
+                    ok;
+                List when is_list (List) ->
+                    ok = mnesia:wait_for_tables([?TABLE], 60000)
+            end;
+        {'EXIT',{aborted,{no_exists,schema,where_to_write}}} ->
+            application:stop(mnesia),
+            mnesia:create_schema([node()]),
+            ok = application:start(mnesia),
+            init_backend(Fields)
+    end.
+
+stop_backend() ->
+    ok.
+
+insert(Session) ->
+    Fun = fun () -> mnesia:write(Session) end,
+    {atomic, ok} = mnesia:transaction(Fun),
+    true.
+
+lookup(Key) ->            
+    Fun = fun () -> mnesia:read(?TABLE, Key) end,
+    {atomic, Result} = mnesia:transaction(Fun),
+    Result.
+
+delete(Key) ->
+    Fun = fun () -> mnesia:delete({?TABLE, Key}) end,
+    {atomic, Result} = mnesia:transaction(Fun),
+    Result.
+
+list() ->
+    Fold = fun (Session, Acc) -> [Session | Acc] end,
+    Fun = fun () -> mnesia:foldl(Fold, [], ?TABLE) end,
+    {atomic, Result} = mnesia:transaction(Fun),
+    Result.
+
+cleanup() ->
+    mnesia:clear_table(?TABLE).
+
+traverse(Gnow) ->
+    Fun = fun() -> tr_traverse(Gnow) end,
+    {atomic, Result} = mnesia:transaction(Fun),
+    Result.
+
+tr_traverse(Gnow) ->
+    tr_traverse(Gnow, mnesia:first(?TABLE)).
+
+tr_traverse(_N, '$end_of_table') ->
+    ok;
+tr_traverse(N, Key) ->
+    case mnesia:read(?TABLE, Key) of
+        [Y] ->
+            case yaws_session_server:has_timedout(Y, N) of
+                false ->
+                    tr_traverse(N, mnesia:next(?TABLE, Key));
+                true ->
+                    yaws_session_server:report_timedout_sess(Y),
+                    Next = mnesia:next(?TABLE, Key),
+                    mnesia:delete({?TABLE, Key}),
+                    tr_traverse(N, Next)                    
+            end;
+        [] ->
+           tr_traverse(N, mnesia:next(?MODULE, Key))
+    end.
+
+
---
+++ test/eunit/yaws_session_server_test.erl
@@ -0,0 +1,181 @@
+-module (yaws_session_server_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-export ([mock_session_server/0]).
+
+%% Our test backend exports
+-export ([init_backend/1, stop_backend/0,
+          insert/1, list/0, lookup/1, delete/1,
+          traverse/1, cleanup/0]).
+
+%% Change this macro to test another backend storage module
+%%-define(BACKEND, ?MODULE).
+-define(BACKEND, yaws_mnesia_session).
+
+start() ->
+    application:load(yaws),
+    application:set_env(yaws, session_back, ?BACKEND),
+    Result = yaws_session_server:start(),
+    ?BACKEND:cleanup(),
+    Result.
+
+init_test() ->
+    {ok, _} = start(),
+    ?assertEqual([], ?BACKEND:list()),
+    yaws_session_server:stop().
+            
+new_session_and_list_test() ->             
+    {ok, _} = start(),
+    ?assertMatch([], ?BACKEND:list()),
+    Cookie1 = yaws_session_server:new_session({opaque, 1}),
+    Cookie2 = yaws_session_server:new_session({opaque, 2}, 1, self()),
+    Cookie3 = yaws_session_server:new_session({opaque, 3}, 1, self(), "cookie"),
+    ?assert(Cookie1 /= Cookie2),
+    "nonode@nohost-" ++ _ = Cookie1,
+    "nonode@nohost-" ++ _ = Cookie2,
+    "cookie" = Cookie3,
+    ?assertMatch([_, _, _], ?BACKEND:list()),
+    yaws_session_server:stop().
+
+cookieval_to_opaque_test() ->
+    {ok, _} = start(),
+    Opaque = {opaque, 1},
+    Cookie1 = yaws_session_server:new_session(Opaque),
+    [Session] = ?BACKEND:list(),
+    Opaque2 = {opaque, 2},
+    true = yaws_session_server:replace_session(Cookie1, Opaque2),
+    [Session_updated] = ?BACKEND:list(),
+    ?assert(Session /= Session_updated),
+    yaws_session_server:stop().
+
+replace_session_test() ->
+    {ok, _} = start(),
+    Opaque = {opaque, 1},
+    Cookie1 = yaws_session_server:new_session(Opaque),
+    [Session] = ?BACKEND:list(),
+    timer:sleep(1000), %% ensure cookie TS is updated of at least 1 second
+    {ok, Result} = yaws_session_server:cookieval_to_opaque(Cookie1),
+    ?assertEqual(Opaque, Result),
+    [Session_updated] = ?BACKEND:list(),
+    ?assert(Session /= Session_updated),
+    yaws_session_server:stop().
+
+delete_session_test() ->
+    {ok, _} = start(),
+    Opaque = {opaque, 1},
+    Cookie1 = yaws_session_server:new_session(Opaque),
+    Cookie2 = yaws_session_server:new_session(Opaque, 10, self()),
+    [_, _] = ?BACKEND:list(),
+    Expected1 = nocleanup,
+    ?assertEqual(Expected1, yaws_session_server:delete_session(Cookie1)),
+    Delete_notif = {yaws_session_end,normal,Cookie2,Opaque},
+    ?assertEqual(Delete_notif, yaws_session_server:delete_session(Cookie2)),
+    [] = ?BACKEND:list(),
+    receive Delete_notif -> ok
+    after 500 -> exit (cleanup_timeout)
+    end,
+    yaws_session_server:stop().
+
+timeout_test () ->
+    {ok, Pid} = start(),
+    Opaque = {opaque, 1},
+    Cookie1 = yaws_session_server:new_session(Opaque, 0, self()),
+    Opaque2 = {opaque, 1},
+    _Cookie2 = yaws_session_server:new_session(Opaque2, 10, self()),
+    ?assertMatch([_, _], ?BACKEND:list()),
+    Pid ! timeout,
+    Timeout_notif = {yaws_session_end,timeout,Cookie1,Opaque},
+    receive Timeout_notif -> ok
+    after 500 -> exit (cleanup_timeout)
+    end,
+    ?assertMatch([_], ?BACKEND:list()),
+    yaws_session_server:stop().
+    
+%% Our test callbacks
+init_backend(_) ->
+    proc_lib:start (?MODULE, mock_session_server, []),
+    ok.
+
+insert(Session) ->
+    ?MODULE ! {insert, Session},
+    true.
+
+lookup(Cookie) ->
+    ?MODULE ! {{lookup, Cookie}, self()},
+    receive {ok, Opaque} -> [Opaque]
+    after 1000 -> list_timeout
+    end.
+
+list() ->
+    ?MODULE ! {list, self()},
+    receive {ok, Sessions} -> Sessions
+    after 1000 -> list_timeout
+    end.
+
+delete(Cookie) ->
+    ?MODULE ! {delete, Cookie},
+    true.
+
+cleanup() ->
+    Cleaner =
+        fun(Session) ->
+                Cookie = yaws_session_server:cookie(Session),
+                delete(Cookie)
+        end,
+    lists:foreach(Cleaner, list()).
+
+traverse(Gnow) ->
+    Timeouter =
+        fun (Session) ->
+                case yaws_session_server:has_timedout(Session, Gnow) of
+                    false ->
+                        ok;
+                    true ->
+                        yaws_session_server:report_timedout_sess(Session),
+                        Cookie = yaws_session_server:cookie(Session),
+                        delete(Cookie)
+                end
+        end,
+    lists:foreach(Timeouter, list()).
+
+stop_backend() ->
+    ?MODULE ! stop,
+    ok.
+
+%% Mock Internals
+mock_session_server() ->
+    register(?MODULE, self()),
+    proc_lib:init_ack({ok, self()}),
+    mock_loop().
+
+mock_loop() ->
+    receive
+        {list, Pid} ->
+            Pid ! {ok, sessions()},
+            mock_loop();
+        {insert, Session} ->
+            Cookie = element(2, Session),
+            put({session, Cookie}, Session),
+            mock_loop();
+        {{lookup, Cookie}, Pid} ->
+            Session = get({session, Cookie}),
+            Pid ! {ok, Session},
+            mock_loop();
+        {delete, Cookie} ->
+            erase({session, Cookie}),
+            mock_loop();
+        stop ->
+            ok;
+        Other ->
+            ?debugFmt("Unexpected: ~p~n", [Other])
+    end.
+
+sessions() ->
+    lists:foldl(fun({{session, _}, Session}, Acc) -> [Session | Acc];
+                    (_, Acc) -> Acc 
+                 end, [], get()).
+
+
+
+
