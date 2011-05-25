fda6d265c47334260a12dd79f9a1be1d110dcca2
Claes Wikstrom <klacke@hyber.org>
2011-05-25 23:32:50 +0200

    FreeBSD patch by Jimmy Olgeni, add quotes around vsn number in .app file

--- applications/wiki/include.mk
+++ applications/wiki/include.mk
@@ -33,7 +33,7 @@ INSTALL_DATA=${INSTALL} -m 644
 
 
 # Hmm, don't know if you are supposed to like this better... ;-)
-APPSCRIPT = '$$vsn=shift; $$mods=""; while(@ARGV){ $$_=shift; s/^([A-Z].*)$$/\'\''$$1\'\''/; $$mods.=", " if $$mods; $$mods .= $$_; } while(<>) { s/\{vsn, git\}/\{vsn, $$vsn\}/; s/\{modules,\[\]\}/\{modules,\[$$mods\]\}/; print; }'
+APPSCRIPT = '$$vsn=shift; $$mods=""; while(@ARGV){ $$_=shift; s/^([A-Z].*)$$/\'\''$$1\'\''/; $$mods.=", " if $$mods; $$mods .= $$_; } while(<>) { s/\{vsn, git\}/\{vsn, "$$vsn"\}/; s/\{modules,\[\]\}/\{modules,\[$$mods\]\}/; print; }'
 
 # Targets
 
