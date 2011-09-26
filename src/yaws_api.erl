26e79bed195dabac7503d3b5378b2fcde7a35095
Steve Vinoski <vinoski@ieee.org>
2011-09-25 22:52:09 -0400

    allow functions to be specified as values in ehtml
    Ehtml now allows functions to be used as values. You can specify a
    function value as an anonymous arity 0 function, or as a tuple
    consisting of a module, function name, and argument list (i.e.,
    {M,F,[Args]}). Functions must return valid ehtml values, and they may
    return other functions as long as those functions eventually return a
    valid non-function ehtml value.
    
    Augment yaws.tex to document this new feature. Add a new eunit test
    for the feature as well.

--- src/yaws_api.erl
+++ src/yaws_api.erl
@@ -1390,11 +1390,12 @@ is_abs_URI1(_) ->
 %% ------------------------------------------------------------
 %% simple erlang term representation of HTML:
 %% EHTML = [EHTML] | {Tag, Attrs, Body} | {Tag, Attrs} | {Tag} |
+%%         {Module, Fun, [Args]} | fun/0
 %%         binary() | character()
-%% Tag          = atom()
+%% Tag   = atom()
 %% Attrs = [{Key, Value}]  or {EventTag, {jscall, FunName, [Args]}}
-%% Key          = atom()
-%% Value = string()
+%% Key   = atom()
+%% Value = string() | {Module, Fun, [Args]} | fun/0
 %% Body  = EHTML
 
 ehtml_expand(Ch) when Ch >= 0, Ch =< 255 -> Ch; %yaws_api:htmlize_char(Ch);
@@ -1408,7 +1409,6 @@ ehtml_expand({ssi,File, Del, Bs}) ->
             X
     end;
 
-
 %%!todo (low priority) - investigate whether tail-recursion would be of any
 %% benefit here instead of the current ehtml_expand(Body) recursion.
 %%                - provide a tail_recursive version & add a file in the
@@ -1417,6 +1417,9 @@ ehtml_expand({ssi,File, Del, Bs}) ->
 ehtml_expand({Tag}) ->
     ["<", atom_to_list(Tag), " />"];
 ehtml_expand({pre_html, X}) -> X;
+ehtml_expand({Mod, Fun, Args})
+  when is_atom(Mod), is_atom(Fun), is_list(Args) ->
+    ehtml_expand(Mod:Fun(Args));
 ehtml_expand({Tag, Attrs}) ->
     NL = ehtml_nl(Tag),
     [NL, "<", atom_to_list(Tag), ehtml_attrs(Attrs), "></",
@@ -1426,8 +1429,9 @@ ehtml_expand({Tag, Attrs, Body}) when is_atom(Tag) ->
     NL = ehtml_nl(Tag),
     [NL, "<", Ts, ehtml_attrs(Attrs), ">", ehtml_expand(Body), "</", Ts, ">"];
 ehtml_expand([H|T]) -> [ehtml_expand(H)|ehtml_expand(T)];
-ehtml_expand([]) -> [].
-
+ehtml_expand([]) -> [];
+ehtml_expand(Fun) when is_function(Fun) ->
+    ehtml_expand(Fun()).
 
 
 ehtml_attrs([]) -> [];
@@ -1435,26 +1439,37 @@ ehtml_attrs([Attribute|Tail]) when is_atom(Attribute) ->
     [[$ |atom_to_list(Attribute)]|ehtml_attrs(Tail)];
 ehtml_attrs([Attribute|Tail]) when is_list(Attribute) ->
     [" ", Attribute|ehtml_attrs(Tail)];
+ehtml_attrs([{Name, {Mod, Fun, Args}} | Tail])
+  when is_atom(Mod), is_atom(Fun), is_list(Args) ->
+    ehtml_attrs([{Name,  Mod:Fun(Args)} | Tail]);
+ehtml_attrs([{Name, Value} | Tail]) when is_function(Value) ->
+    ehtml_attrs([{Name, Value()} | Tail]);
 ehtml_attrs([{Name, Value} | Tail]) ->
-    ValueString = if is_atom(Value) -> [$",atom_to_list(Value),$"];
-                     is_list(Value) -> [$",Value,$"];
-                     is_integer(Value) -> [$",integer_to_list(Value),$"];
-                     is_float(Value) -> [$",float_to_list(Value),$"]
-                  end,
+    ValueString = [$", if
+                           is_atom(Value) -> atom_to_list(Value);
+                           is_list(Value) -> Value;
+                           is_integer(Value) -> integer_to_list(Value);
+                           is_float(Value) -> float_to_list(Value)
+                       end, $"],
     [[$ |atom_to_list(Name)], [$=|ValueString]|ehtml_attrs(Tail)];
+ehtml_attrs([{check, Name, {Mod, Fun, Args}} | Tail])
+  when is_atom(Mod), is_atom(Fun), is_list(Args) ->
+    ehtml_attrs([{check, Name,  Mod:Fun(Args)} | Tail]);
+ehtml_attrs([{check, Name, Value} | Tail]) when is_function(Value) ->
+    ehtml_attrs([{check, Name, Value()} | Tail]);
 ehtml_attrs([{check, Name, Value} | Tail]) ->
-    ValueString = if is_atom(Value) -> [$",atom_to_list(Value),$"];
-                     is_list(Value) ->
-                          Q = case deepmember($", Value) of
-                                  true -> $';
-                                  false -> $"
-                              end,
-                          [Q,Value,Q];
-                     is_integer(Value) -> [$",integer_to_list(Value),$"];
-                     is_float(Value) -> [$",float_to_list(Value),$"]
-                   end,
-                   [[$ |atom_to_list(Name)],
-                    [$=|ValueString]|ehtml_attrs(Tail)].
+    Val = if
+              is_atom(Value) -> atom_to_list(Value);
+              is_list(Value) -> Value;
+              is_integer(Value) -> integer_to_list(Value);
+              is_float(Value) -> float_to_list(Value)
+          end,
+    Q = case deepmember($", Val) of
+            true -> $';
+            false -> $"
+        end,
+    ValueString = [Q,Value,Q],
+    [[$ |atom_to_list(Name)], [$=|ValueString]|ehtml_attrs(Tail)].
 
 
 
