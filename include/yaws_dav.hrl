79a01d0f86e65e419c569b7755bca1b66ab75d66
tjeerd <tjeerd@yolt.nl>
2012-08-23 20:30:09 +0200

    changed some comment

--- include/yaws_dav.hrl
+++ include/yaws_dav.hrl
@@ -1,7 +1,7 @@
 -ifndef(_YAWS_DAV).
 -define(_YAWS_DAV, true).
 
--define(LOCK_LIFETIME, 120). % lock lifetime in seconds: 2 minutes
+-define(LOCK_LIFETIME, 900). % lock lifetime in seconds: 15 minutes
 -define(CLEANUP_INTERVAL, 60). % cleanup interval in seconds: 1 minute
 
 -define(elog(X,Y), error_logger:info_msg("*elog ~p:~p: " X,
@@ -17,7 +17,7 @@
             owner=anonymous,    % lock owner if submitted
             depth=infinity,     % 0|infinity
             scope=exclusive,    % exclusive|shared
-            type=write,         % RFC4918 specifies only one type
+            type=write,         % write
             timeout=0,          % ?LOCK_LIFETIME or shorter
             timestamp=0         % erlang:now()
         }).
