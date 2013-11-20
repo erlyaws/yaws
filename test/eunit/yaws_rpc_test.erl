c978fe03bbc6b4f4650df70a2a528a68449dac68
Lastres <ramonlastres@gmail.com>
2013-11-20 16:57:17 +0000

    Change binary conversion function in yaws_rpc in order to deal with unicode characters

---
+++ test/eunit/yaws_rpc_test.erl
@@ -0,0 +1,23 @@
+%% coding: utf-8
+-module(yaws_rpc_test).
+-compile(export_all).
+
+-include_lib("eunit/include/eunit.hrl").
+-include("../../include/yaws_api.hrl").
+
+-define(PARAMS, ["åäö","abc"]).
+
+%% ignore it
+unicode_test() ->
+    Payload = json2:encode({struct,[{method,"foo"},{id,"id"},
+                                    {params,?PARAMS}]}),
+    Req = #http_request{method='POST', version={1,1}},
+    Hdrs = #headers{},
+    Arg = #arg{clidata=Payload,req=Req,headers=Hdrs},
+    yaws_rpc:handler(Arg, {?MODULE, handler}),
+    timer:sleep(2000),
+    ok.
+
+handler(_, {call,foo,{array,?PARAMS}}) ->
+    false.
+
