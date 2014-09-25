e6e0ded6fd8695c8e04c7657a43c0a4adf33b0e9
Steve Vinoski <vinoski@ieee.org>
2014-09-25 17:22:42 -0400

    Fix #187: handle EINPROGRESS and EALREADY in sendfile driver
    Thanks to Maas-Maarten Zeeman for reporting this.

--- c_src/yaws_sendfile_drv.c
+++ c_src/yaws_sendfile_drv.c
@@ -258,7 +258,8 @@ static void yaws_sendfile_drv_ready_output(ErlDrvData handle, ErlDrvEvent ev)
     cur_offset = xfer->offset;
     result = yaws_sendfile_call(sfd->socket_fd, xfer->file_fd,
                                 &xfer->offset, xfer->count);
-    if (result < 0 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
+    if (result < 0 && (errno == EAGAIN || errno == EWOULDBLOCK ||
+                       errno == EINPROGRESS || errno == EALREADY)) {
         if (xfer->offset != cur_offset) {
             off_t written = xfer->offset - cur_offset;
             xfer->count -= written;
