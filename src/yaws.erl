9120d0c4413f2fdba4e09c8721640b31aff9c8ea
Steve Vinoski <vinoski@ieee.org>
2019-04-20 07:57:27 -0400

    Support strip_undefined_bindings in set_sc_flags

--- src/yaws.erl
+++ src/yaws.erl
@@ -807,6 +807,8 @@ set_sc_flags([{forward_proxy, Bool}|T], Flags) ->
     set_sc_flags(T, flag(Flags, ?SC_FORWARD_PROXY, Bool));
 set_sc_flags([{auth_skip_docroot, Bool}|T], Flags) ->
     set_sc_flags(T, flag(Flags, ?SC_AUTH_SKIP_DOCROOT, Bool));
+set_sc_flags([{strip_undefined_bindings, Bool}|T], Flags) ->
+    set_sc_flags(T, flag(Flags, ?SC_STRIP_UNDEF_BINDINGS, Bool));
 set_sc_flags([_Unknown|T], Flags) ->
     error_logger:format("Unknown and unhandled flag ~p~n", [_Unknown]),
     set_sc_flags(T, Flags);
