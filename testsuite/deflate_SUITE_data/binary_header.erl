826f76990506535a7b2b8aa6fa88d4601a4d9142
Steve Vinoski <vinoski@ieee.org>
2020-04-20 06:46:15 -0400

    Update github repository URL
    The Yaws github repository URL has changed to
    
    https://github.com/erlyaws/yaws
    
    Change occurrences of the old URL to the new one.
    
    Note that the file rebar-templates/yawsnode.rebar.config still has an
    occurrence of the old URL; this is because this file will be going
    away soon when the rebar3-support branch merges to master.

--- testsuite/deflate_SUITE_data/binary_header.erl
+++ testsuite/deflate_SUITE_data/binary_header.erl
@@ -4,7 +4,7 @@
 out(_Arg) ->
     %% Regression test for github issue 367, where returning a binary
     %% header with deflate on would cause a crash.
-    %% https://github.com/klacke/yaws/issues/367
+    %% https://github.com/erlyaws/yaws/issues/367
     [{status, 200},
      {header, {content_type, <<"application/octet">>}},
      {ehtml, {pre,[],<<"ABCDEFGHIJKLMNOPQRSTUVWXYZ\n">>}}].
