28eecfd1c65c369de5b4b99cea9407205bbe8f8e
Steve Vinoski <vinoski@ieee.org>
2020-05-14 08:13:49 -0400

    Add a WARNINGS_AS_ERRORS make variable
    By default, WARNINGS_AS_ERRORS is set to -Werror to cause erlc to
    treat warnings as errors. The WARNINGS_AS_ERRORS variable allows this
    to be easily turned off when running make:
    
        make all WARNINGS_AS_ERRORS=

--- include.mk
+++ include.mk
@@ -3,7 +3,9 @@ am__v_ERLC_ = $(am__v_ERLC_$(AM_DEFAULT_VERBOSITY))
 am__v_ERLC_0 = @echo "  ERLC    " $@;
 am__v_ERLC_1 =
 
-ERLC_GENERIC_FLAGS = -Werror  +debug_info $(DEBUG_ERLC_FLAGS)				\
+WARNINGS_AS_ERRORS = -Werror
+
+ERLC_GENERIC_FLAGS = $(WARNINGS_AS_ERRORS) +debug_info $(DEBUG_ERLC_FLAGS)		\
 		     -pa $(top_srcdir) -pa $(top_builddir) -pa $(top_builddir)/ebin	\
 		     -I $(top_srcdir)/include -I $(srcdir)/../include 			\
 		     -I $(top_builddir)/include -I $(builddir)/../include
