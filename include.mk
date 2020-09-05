1c8f4a8597dfd56ad49b8e3d7831b14de4a63a20
Steve Vinoski <vinoski@ieee.org>
2020-09-05 09:02:20 -0400

    Add OTP 23.0 and drop 17.x
    Drop support for 17.x and set the minimum supported OTP version to
    18.0. Change references to 17.0 to 18.0 in documentation and HTML
    pages.
    
    In OTP 23 we get a warning for the deprecated ssl:cipher_suites/0
    function. For now, turn off -Werror by default; it will be reenabled
    once the warning is fixed. If you want to build with warnings turned
    on, use "make all WARNINGS_AS_ERRORS=-Werror".
    
    Add 23.0 to the Travis-CI YAML file, and remove 17.x from it.

--- include.mk
+++ include.mk
@@ -3,7 +3,10 @@ am__v_ERLC_ = $(am__v_ERLC_$(AM_DEFAULT_VERBOSITY))
 am__v_ERLC_0 = @echo "  ERLC    " $@;
 am__v_ERLC_1 =
 
-WARNINGS_AS_ERRORS = -Werror
+# TODO -Werror is turned off until the deprecated ssl:cipher_suites/0
+# can be fixed
+#WARNINGS_AS_ERRORS = -Werror
+WARNINGS_AS_ERRORS =
 
 ERLC_GENERIC_FLAGS = $(WARNINGS_AS_ERRORS) +debug_info $(DEBUG_ERLC_FLAGS)		\
 		     -pa $(top_srcdir) -pa $(top_builddir) -pa $(top_builddir)/ebin	\
