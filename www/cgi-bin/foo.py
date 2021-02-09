1330c1ab3c8f6aea7944332cde09a0d7b8479acf
Vladislav Glinsky <cl0ne@mithril.org.ua>
2021-02-09 14:23:34 +0200

    Python fixes
    Use env for python file shebangs.
    
    For python2/python3 compatibility, import __future__.print_function
    for portable python3 print syntax.

--- www/cgi-bin/foo.py
+++ www/cgi-bin/foo.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
+from __future__ import print_function
 
 import cgi
 
-print "Content-type: text/html\n\n"
-
-print "hi there "
-
+print("Content-type: text/html\n\n")
+print("<h1>hi there</h1>")
