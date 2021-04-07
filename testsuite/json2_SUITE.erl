ca704a916ee05113564668cf5281700db25021f3
Fabian N.C. van 't Hooft <fnchooft@gmail.com>
2021-04-07 00:24:17 -0300

    Add convenience function json2:obj_fetch/3
    The json2:obj_fetch/2 either returns an object member's value or exits
    with a struct_no_key error if the object member is not found. Add
    json2:obj_fetch/3 that returns either the object member's value or a
    default value if the object member is not found.
    
    Add a new unit test for json2:obj_fetch/2,3 to the json2_SUITE.
    
    Also in json2_SUITE, fix the test source directories for the
    json2_decode_valid and json2_decode_invalid tests. Add asserts to
    verify that one or more test files are processed by those tests.

--- testsuite/json2_SUITE.erl
+++ testsuite/json2_SUITE.erl
@@ -8,7 +8,8 @@ all() ->
     [
      json2_decode_valid,
      json2_decode_invalid,
-     json2_decode_invalid_utf8
+     json2_decode_invalid_utf8,
+     json2_obj_fetch
     ].
 
 groups() ->
@@ -36,8 +37,9 @@ end_per_testcase(_Test, _Config) ->
 
 %%====================================================================
 json2_decode_valid(_Config) ->
-    Dir = filename:join(?srcdir, "json2_SUITE_data/valid"),
+    Dir = filename:join(?data_srcdir(?MODULE), "valid"),
     Tests = filelib:wildcard(filename:join(Dir, "*.json")),
+    ?assertMatch([_|_], Tests),
     [begin
          {ok, Bin} = file:read_file(T),
          Str = unicode:characters_to_list(Bin),
@@ -46,8 +48,9 @@ json2_decode_valid(_Config) ->
     ok.
 
 json2_decode_invalid(_Config) ->
-    Dir = filename:join(?srcdir, "json2_SUITE_data/invalid"),
+    Dir = filename:join(?data_srcdir(?MODULE), "invalid"),
     Tests = filelib:wildcard(filename:join(Dir, "*.json")),
+    ?assertMatch([_|_], Tests),
     [begin
          {ok, Bin} = file:read_file(T),
          Str = unicode:characters_to_list(Bin),
@@ -60,4 +63,20 @@ json2_decode_invalid_utf8(_Config) ->
     Str2 = [$", 16#dc49, $"],  %% low surrogate only
     ?assertMatch({Str1, {error, _}}, {Str1, json2:decode_string(Str1)}),
     ?assertMatch({Str2, {error, _}}, {Str2, json2:decode_string(Str2)}),
+    ok.
+
+json2_obj_fetch(_Config) ->
+    F = filename:join(?data_srcdir(?MODULE), "valid/simple-object.json"),
+    {ok, Bin} = file:read_file(F),
+    Str = unicode:characters_to_list(Bin),
+    {ok, Obj} = json2:decode_string(Str),
+    ?assertEqual({array,[]}, json2:obj_fetch("a", Obj)),
+    NotFound = try
+                   json2:obj_fetch("b", Obj)
+               catch
+                   C:Exc ->
+                       {C, Exc}
+               end,
+    ?assertEqual({exit, {struct_no_key, "b"}}, NotFound),
+    ?assertEqual(foo, json2:obj_fetch("b", Obj, foo)),
     ok.
