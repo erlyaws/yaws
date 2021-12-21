erlyaws/yaws

commit 9cd9173aefe42e949afa3a9230bd6916954f9995

Allow colons in dir listing file references
When encoding filenames for use as relative URLs in directory
listings, if a file contains only colons as encoded characters, use
the original filename prefixed with "./" as its relative URL as per
RFC3986 section 4.2.

src/yaws_api.erl
@@ -30,7 +30,7 @@
          htmlize/1, htmlize_char/1, f/2, fl/1]).
 -export([find_cookie_val/2, secs/0,
          url_decode/1, url_decode_q_split/1, url_decode_with_encoding/2,
-         url_encode/1, parse_url/1, parse_url/2, format_url/1,
+         url_encode/1, url_encode/2, parse_url/1, parse_url/2, format_url/1,
          format_partial_url/2]).
 -export([is_absolute_URI/1]).
 -export([path_norm/1, path_norm_reverse/1,
@@ -910,18 +910,30 @@ url_decode_q_split([], Ack) ->
     {path_norm_reverse(Ack), []}.
 
 
-url_encode(URL) when is_list(URL) ->
+url_encode(URL) ->
+    {Res, _} = url_encode(URL, return_encoded),
+    Res.
+url_encode(URL, return_encoded) when is_list(URL) ->
     Bin = case io_lib:latin1_char_list(URL) of
               true  -> list_to_binary(URL);
               false -> unicode:characters_to_binary(URL)
           end,
-    url_encode(Bin);
-url_encode(URL) when is_binary(URL) ->
+    url_encode(Bin, return_encoded);
+url_encode(URL, return_encoded) when is_binary(URL) ->
     %% ReservedChars = "!*'();:@&=+$,/?%#[]",
     UnreservedChars = sets:from_list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                                      "abcdefghijklmnopqrstuvwxyz"
                                      "0123456789-_.~"),
-    lists:flatten([url_encode_byte(Byte, UnreservedChars) || <<Byte>> <= URL]).
+    {Bytes, Encoded} = lists:foldl(
+			 fun(Byte, {Bytes, Encoded}) ->
+				 case url_encode_byte(Byte, UnreservedChars) of
+				     Byte ->
+					 {[Byte|Bytes], Encoded};
+				     Enc ->
+					 {[Enc|Bytes], sets:add_element(Byte, Encoded)}
+				 end
+			 end, {[], sets:new()}, [Byte || <<Byte>> <= URL]),
+    {lists:flatten(Bytes), sets:to_list(Encoded)}.
 
 url_encode_byte(Byte, UnreservedChars) ->
     case sets:is_element(Byte, UnreservedChars) of

src/yaws_ls.erl
@@ -420,6 +420,24 @@ file_entry({ok, FI}, _DirName, Name, Qry, Descriptions) ->
     EncName  = file_display_name(Name),
     Description = get_description(Name,Descriptions),
 
+    %% URL-encode the filename. As far as URLs go, the filename is a
+    %% relative-path reference. RFC3986 section 4.2 states:
+    %%
+    %%  A path segment that contains a colon character (e.g.,
+    %%  "this:that") cannot be used as the first segment of a
+    %%  relative-path reference, as it would be mistaken for a scheme
+    %%  name. Such a segment must be preceded by a dot-segment (e.g.,
+    %%  "./this:that") to make a relative-path reference.
+    %%
+    %% To avoid encoding colon characters in file references, since
+    %% browsers won't treat them as references in that case, if
+    %% yaws_api:url_encode/2 indicates that only colon characters were
+    %% encoded for a filename, we use Name for the reference but
+    %% prepend "./" to it as per the RFC.
+    HRef = case yaws_api:url_encode(Name, return_encoded) of
+               {_, [$:]} -> "./" ++ Name;
+               {HRef0, _} -> HRef0
+           end,
     Entry =
         ?F("  <tr>\n"
            "    <td><img src=~p alt=~p/><a href=~p title=\"~ts\">~ts</a></td>\n"
@@ -429,7 +447,7 @@ file_entry({ok, FI}, _DirName, Name, Qry, Descriptions) ->
            "  </tr>\n",
            ["/icons/" ++ Gif,
             Alt,
-            yaws_api:url_encode(Name) ++ QryStr,
+            HRef ++ QryStr,
             list_to_binary(EncName),
             list_to_binary(trim(EncName,?FILE_LEN_SZ)),
             datestr(FI),

commit ea19a520b67aa0d1e5001007d6899898d0f23991

Fix url_encode issue
Commit 9cd9173a introduced a problem with yaws_api:url_encode/1 that
wasn't caught because "make check" sometimes reports success on macOS
even when a failure occurs.

Fix verified on Ubuntu and macOS.

src/yaws_api.erl
@@ -933,7 +933,7 @@ url_encode(URL, return_encoded) when is_binary(URL) ->
 					 {[Enc|Bytes], sets:add_element(Byte, Encoded)}
 				 end
 			 end, {[], sets:new()}, [Byte || <<Byte>> <= URL]),
-    {lists:flatten(Bytes), sets:to_list(Encoded)}.
+    {lists:flatten(lists:reverse(Bytes)), sets:to_list(Encoded)}.
 
 url_encode_byte(Byte, UnreservedChars) ->
     case sets:is_element(Byte, UnreservedChars) of